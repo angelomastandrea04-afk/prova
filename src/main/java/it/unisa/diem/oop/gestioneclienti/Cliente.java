803082afa7f6eab8518f3e72b36eae8a633965ca
angelo <angelo@172.19.245.57>
2025-11-10 10:26:09 +0100

    Revert "Revert """
    This reverts commit 7e1aeff438b5185ad6a9b1fe4815756cda86149a.

--- src/main/java/it/unisa/diem/oop/gestioneclienti/Cliente.java
+++ src/main/java/it/unisa/diem/oop/gestioneclienti/Cliente.java
@@ -14,6 +14,7 @@ public class Cliente {
     private final String cognome;
     private final String codiceFiscale;
     private final String indirizzo;
+    private final String gay="gayz";
     
     public int SoloConTeSospiro() {
         return 69;
