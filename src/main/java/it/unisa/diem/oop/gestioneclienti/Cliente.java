533981e183c63329fc4169b19be35e10c98cd25a
_Sweet.Meet_ <martavalenza0@gmail.com>
2025-11-12 17:45:19 +0100

    Update Cliente.java

--- src/main/java/it/unisa/diem/oop/gestioneclienti/Cliente.java
+++ src/main/java/it/unisa/diem/oop/gestioneclienti/Cliente.java
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 package it.unisa.diem.oop.gestioneclienti;
-
+/*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*/
 /**
  *
  * @author angelo
@@ -42,3 +42,4 @@ public class Cliente {
                 codiceFiscale + " | Indirizzo: " + indirizzo);
     }
 }
+
